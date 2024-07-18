2c0fcb904338d12d1ca7dcfe054ca779c7646c45
jujudev7 <penna.julien@gmail.com>
2024-07-18 16:22:25 +0200

    page.js modified

--- app/page.js
+++ app/page.js
@@ -1,95 +1,102 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import { CreateSimpleJSXElement } from "./pages/1-create-simple-jsx-element";
+import { CreateComplexJSXElement } from "./pages/2-create-complex-jsx-element";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
+    <div>
+      <h1>1. Create a Simple JSX Element</h1>
+      <p>
+        React is an Open Source view library created and maintained by Facebook.
+        It's a great tool to render the User Interface (UI) of modern web
+        applications. React uses a syntax extension of JavaScript called JSX
+        that allows you to write HTML directly within JavaScript.{" "}
+      </p>
+      <p>
+        This has several benefits. It lets you use the full programmatic power
+        of JavaScript within HTML, and helps to keep your code readable. For the
+        most part, JSX is similar to the HTML that you have already learned,
+        however there are a few key differences that will be covered throughout
+        these challenges.{" "}
+      </p>
+      <p>
+        For instance, because JSX is a syntactic extension of JavaScript, you
+        can actually write JavaScript directly within JSX. To do this, you
+        simply include the code you want to be treated as JavaScript within
+        curly braces: {"this is treated as JavaScript code"}. Keep this in mind,
+        since it's used in several future challenges.{" "}
+      </p>
+      <p>
+        However, because JSX is not valid JavaScript, JSX code must be compiled
+        into JavaScript. The transpiler Babel is a popular tool for this
+        process. For your convenience, it's already added behind the scenes for
+        these challenges. If you happen to write syntactically invalid JSX, you
+        will see the first test in these challenges fail.{" "}
+      </p>{" "}
+      <p>
+        It's worth noting that under the hood the challenges are calling
+        ReactDOM.render(JSX, document.getElementById('root')). This function
+        call is what places your JSX into React's own lightweight representation
+        of the DOM. React then uses snapshots of its own DOM to optimize
+        updating only specific parts of the actual DOM.
+      </p>
+      <p>
+        The current code uses JSX to assign a div element to the constant JSX.
+        Replace the div with an h1 element and add the text Hello JSX! inside
+        it.
+      </p>
+      <CreateSimpleJSXElement />
+      <div>
+        <h1>2. Create a Complex JSX Element</h1>
         <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
+          One important thing to know about nested JSX is that it must return a
+          single element. This one parent element would wrap all of the other
+          levels of nested elements. For instance, several JSX elements written
+          as siblings with no parent wrapper element will not transpile.
         </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+        <p>Here's an example:</p>
+        <code>
+          Valid JSX:
+          <p>&lt;p&gt;Paragraph One&lt;/p&gt;</p>
+          <p>&lt;p&gt;Paragraph Two&lt;/p&gt;</p>
+          <p>&lt;p&gt;Paragraph Three&lt;/p&gt;</p>
+          <br />
+          Invalid JSX:
+          <p>&lt;p&gt;Paragraph One&lt;/p&gt;</p>
+          <p>&lt;p&gt;Paragraph Two&lt;/p&gt;</p>
+          <p>&lt;p&gt;Paragraph Three&lt;/p&gt;</p>
+        </code>
+        <br />
+        <p>
+          Define a new constant JSX that renders a div which contains the
+          following elements in order: An h1, a p, and an unordered list that
+          contains three li items. You can include any text you want within each
+          element. Note: When rendering multiple elements like this, you can
+          wrap them all in parentheses, but it's not strictly required.
+        </p>{" "}
+        <p>
+          Also notice this challenge uses a div tag to wrap all the child
+          elements within a single parent element. If you remove the div, the
+          JSX will no longer transpile. Keep this in mind, since it will also
+          apply when you return JSX elements in React components.
+        </p>
+        <code>
+          const JSX = (
+          <div>
+            &lt;div&gt; <p> &lt;h1&gt;Title&lt;/h1&gt;</p>
+            <p>&lt;p&gt;Paragraph&lt;/p&gt;</p>
+            <ul>
+              &lt;ul&gt;
+              <li>&lt;li&gt;A&lt;/li&gt;</li>
+              <li>&lt;li&gt;B&lt;/li&gt;</li> <li>&lt;li&gt;C&lt;/li&gt;</li>{" "}
+              &lt;/ul&gt;
+            </ul>
+            &lt;/div&gt;
+          </div>
+          );
+        </code>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <br />
+      <CreateComplexJSXElement />
+    </div>
   );
 }
